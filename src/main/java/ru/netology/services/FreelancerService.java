9e6b4b197e0dcb35bbb03ce2fc19697de9c423ca
Art test <vis-buch@yandex.ru>
2026-01-18 17:22:09 +0300

    Finalization of corrections

--- src/main/java/ru/netology/services/FreelancerService.java
+++ src/main/java/ru/netology/services/FreelancerService.java
@@ -2,13 +2,13 @@ package ru.netology.services;
 
 public class FreelancerService {
     public int calculate(int income, int expenses, int threshold) {
-        int count = 0; // счётчик месяцев отдыха
-        int money = 0; // количество денег на счету
+        int count = 0;
+        int money = 0;
         for (int month = 1; month < 12; month++) {
-            if (money >= threshold) { // можем ли отдыхать?
-                count++; // увеличиваем счётчик месяцев отдыха
+            if (money >= threshold) {
                 money = money - expenses;
-                money = money - money/3;
+                money = money / 3;
+                count++;
             } else {
                 money = money + income;
                 money = money - expenses;
